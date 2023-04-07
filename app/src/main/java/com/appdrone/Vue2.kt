fa0f476327dfe69f20fa15233ddeadb41d8a40e0
FloFloZone <misteurfl@gmail.com>
2023-04-07 16:06:30 +0200

    fin vue 2

--- app/src/main/java/com/appdrone/Vue2.kt
+++ app/src/main/java/com/appdrone/Vue2.kt
@@ -5,21 +5,20 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.StrictMode
 import android.widget.Button
 import android.widget.SeekBar
-
+import androidx.appcompat.app.AppCompatActivity
+import com.appdrone.databinding.ActivityVue2Binding
+import com.appdrone.entities.Drone
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.appdrone.databinding.ActivityVue2Binding
-import com.appdrone.entities.Drone
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.PolylineOptions
 import com.google.maps.android.SphericalUtil
 import java.util.*
@@ -32,13 +31,18 @@ class Vue2 : AppCompatActivity(), OnMapReadyCallback, SensorEventListener {
     val timer = Timer()
     val timer2 = Timer()
 
+    // Création drone
+    var drone: Drone = Drone("Drone")
+
+    // On initialise certaines variables du drone ici pour pouvoir y accéder de partout
+    // On crée également des booleans permettant de gérer plusieurs actions
     private var doFirst: Boolean = false
     private var timerBool = true
     private var angle = 0.0
     private var vitesseGlobale = 0.0
     private var isGauche: Int = 1
     private lateinit var sensorManager: SensorManager
-    private var home: LatLng = LatLng(46.15507352200837, -1.154131833798576)
+    private var home: LatLng = LatLng(46.147149, -1.168021)
     private var positionGlobale: LatLng = home
 
     private var stopAccelerometre: Boolean = false
@@ -54,17 +58,20 @@ class Vue2 : AppCompatActivity(), OnMapReadyCallback, SensorEventListener {
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
-        // seekbar vitesse
+        // boutons home & urgence ainsi que seekbar pour changer la vitesse
+        val buttonHome = findViewById<Button>(R.id.buttonHome)
+        val buttonUrgence = findViewById<Button>(R.id.buttonUrgence)
+
         val vitesseBar: SeekBar = findViewById(R.id.vitessebar)
         vitesseBar.min = 0
         vitesseBar.max = 70
 
+            // Changement de la vitesse selon la valeur de la seekbar
         vitesseBar.setOnSeekBarChangeListener(object :
             SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seek: SeekBar?, progress: Int, fromuser: Boolean) {
                 vitesseGlobale = progress.toDouble()
             }
-
 
             // fonctions obligatoires à implémenter pour l'interface
             override fun onStartTrackingTouch(p0: SeekBar?) {
@@ -73,121 +80,77 @@ class Vue2 : AppCompatActivity(), OnMapReadyCallback, SensorEventListener {
             override fun onStopTrackingTouch(p0: SeekBar?) {
             }
         })
-        // boutons + listeners
 
-        val buttonHome = findViewById<Button>(R.id.buttonHome)
-        val buttonUrgence = findViewById<Button>(R.id.buttonUrgence)
 
+        // Listener du boutton Home
         buttonHome.setOnClickListener {
-            println("Home !")
-            val distanceLong = home.longitude - positionGlobale.longitude
+            // On calcule l'angle entre la position actuelle et la position de départ
+            val lat1 = Math.toRadians(positionGlobale.latitude)
+            val lon1 = Math.toRadians(positionGlobale.longitude)
+            val lat2 = Math.toRadians(home.latitude)
+            val lon2 = Math.toRadians(home.longitude)
 
-            /*
-            val angleDirection = Math.toDegrees(Math.atan2(distanceLat, distanceLong))
-            println("lat : " + distanceLat + " long : " + distanceLong)
-            println("direction : " + angleDirection + " | angle : " + angle)
+            val y = Math.sin(lon2 - lon1) * Math.cos(lat2)
+            val x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(lon2 - lon1)
 
-            */
+            val bearing = Math.toDegrees(Math.atan2(y, x)).toFloat()
+            val temp = (bearing+360)%360
 
-            // Position initiale (latitude et longitude)
-            val lat1 = 46.16025161517004
-            val lon1 = -1.156863590985258
-
-// Position d'arrivée (latitude et longitude)
-            val lat2 = 46.15507352200837
-            val lon2 = -1.154131833798576
-
-// Rayon de la Terre en kilomètres
-            val R = 6371.0
-
-// Calcul de la différence de longitude
-            val deltaLon = Math.toRadians(lon2 - lon1)
-
-// Calcul de la distance entre les deux points en kilomètres
-            val lat1Radians = Math.toRadians(lat1)
-            val lat2Radians = Math.toRadians(lat2)
-            val deltaLat = lat2Radians - lat1Radians
-            val a =
-                sin(deltaLat / 2).pow(2.0) + cos(lat1Radians) * cos(lat2Radians) * sin(deltaLon / 2).pow(
-                    2.0
-                )
-            val c = 2 * atan2(sqrt(a), sqrt(1 - a))
-            val distance = R * c
-
-// Calcul de l'angle en radians
-            val y = sin(deltaLon) * cos(lat2Radians)
-            val x = cos(lat1Radians) * sin(lat2Radians) - sin(lat1Radians) * cos(lat2Radians) * cos(
-                deltaLon
-            )
-            var angleDirection = atan2(y, x)
-
-// Conversion en degrés
-            angleDirection = Math.toDegrees(angleDirection)
-            if (angleDirection < 0) {
-                angleDirection += 360
-            }
-
-            angleDirection = (angleDirection + 180) % 360
-
-            println("Angle: $angleDirection")
-            println("direction : " + angleDirection + " | angle : " + angle)
-            stopAccelerometre = true
-            angle = angleDirection
+            // Une fois l'angle trouvé, on tourne le bateau dans cette direction pour qu'il retourne à la position de départ
+            angle = (temp).toDouble()
         }
 
+        // Listener du boutton Urgence : on met la vitesse à 0, tout comme la position visuelle de la seekbar
         buttonUrgence.setOnClickListener {
             println("Urgence !")
             vitesseGlobale = 0.0
-            vitesseBar.setProgress(0)
+            vitesseBar.progress = 0
         }
 
-
+        // Démarage et setup de l'accelerometre
         setUpSensorStuff()
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        //Create a data source and add it to the map.
         mMap = googleMap
-
         val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
         StrictMode.setThreadPolicy(policy)
         val polylineOptions = PolylineOptions().color(Color.RED).width(5f)
 
-        // Création drone
-        var drone1: Drone = Drone("Principal")
-        // Position : vieux port
-        drone1.position!!.latitude = home.latitude
-        drone1.position!!.longitude = home.longitude
-        drone1.orientation = 0.0
-        drone1.vitesse = 0.0
 
+        // Position de départ : dans le vieux port de La Rochelle
+        drone.position!!.latitude = home.latitude
+        drone.position!!.longitude = home.longitude
+        drone.orientation = 0.0
+        drone.vitesse = 0.0
 
+        // On ajoute un marqueur sur la carte pour représenter le bateau
         val markerbateau = mMap.addMarker(
             MarkerOptions()
                 .position(LatLng(0.00, 0.00))
-                .title(drone1.name)
+                .title(drone.name)
                 .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon)).snippet("")
         )
         if (markerbateau != null) {
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(markerbateau.position, 8f))
         }
-
+        // Première task : affiche le bateau sur la carte, en utilisant les variables globales
         val task = object : TimerTask() {
             override fun run() {
-                drone1.orientation = angle
-                drone1.vitesse = vitesseGlobale
-                drone1.position!!.latitude = positionGlobale.latitude
-                drone1.position!!.longitude = positionGlobale.longitude
+                drone.orientation = angle
+                drone.vitesse = vitesseGlobale
+                drone.position!!.latitude = positionGlobale.latitude
+                drone.position!!.longitude = positionGlobale.longitude
 
                 this@Vue2.runOnUiThread {
                     if (markerbateau != null) {
                         markerbateau.position =
-                            LatLng(drone1.position!!.latitude, drone1.position!!.longitude)
-                        markerbateau.rotation = drone1.orientation!!.toFloat()
+                            LatLng(drone.position!!.latitude, drone.position!!.longitude)
+                        markerbateau.rotation = drone.orientation!!.toFloat()
 
                         markerbateau.snippet =
-                            "Vitesse : " + drone1.vitesse + " knots" + " -> " + Math.round(
-                                drone1.vitesse!! * 1.852
+                            "Vitesse : " + drone.vitesse + " knots" + " -> " + Math.round(
+                                drone.vitesse!! * 1.852
                             ) + " km/h"
                         mMap.moveCamera(CameraUpdateFactory.newLatLng(markerbateau.position))
                         polylineOptions.add(markerbateau.position)
@@ -203,16 +166,16 @@ class Vue2 : AppCompatActivity(), OnMapReadyCallback, SensorEventListener {
             }
         }
 
-        // timer 2 get angle new pos
+        // Deuxième task : permet de mettre à jour la position du bateau toutes les secondes, en modifiant les variables globales
         val task2 = object : TimerTask() {
             override fun run() {
                 // knot -> m/s
-                val metresecondes: Double = drone1.vitesse!! / 1.94384
+                val metresecondes: Double = drone.vitesse!! / 1.94384
                 val nvPos: LatLng = getDestinationLatLng(
                     LatLng(
-                        drone1.position!!.latitude,
-                        drone1.position!!.longitude
-                    ), metresecondes, drone1.orientation!!
+                        drone.position!!.latitude,
+                        drone.position!!.longitude
+                    ), metresecondes, drone.orientation!!
                 )
                 positionGlobale = nvPos
 
@@ -224,6 +187,7 @@ class Vue2 : AppCompatActivity(), OnMapReadyCallback, SensorEventListener {
     }
 
 
+    // Mise en place de l'accéléromètre
     private fun setUpSensorStuff() {
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
         sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also {
@@ -231,14 +195,16 @@ class Vue2 : AppCompatActivity(), OnMapReadyCallback, SensorEventListener {
         }
     }
 
+    // Listener de l'accéléromètre
     override fun onSensorChanged(event: SensorEvent?) {
         if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
             if (timerBool && !stopAccelerometre) { // Si une seconde est passée et que bouton Home pas appuyé
-                if (!doFirst) {
+                if (!doFirst) { // A l'arrivée sur la Vue, on retient l'orientation du téléphone
                     if (event.values[0] < 0) isGauche = -1
                     println("isGauche set to " + isGauche)
                     doFirst = true
                 } else {
+                    // Toutes les fois suivantes : on calcule le nouvel angle en fonction  de la rotation du téléphone et de la vitesse
                     val accel: Float = event.values[1]
                     var diviseurVitesse: Int = (16 - (vitesseGlobale / 2.33)).toInt()
                     if (diviseurVitesse < 1) diviseurVitesse = 1
@@ -253,10 +219,12 @@ class Vue2 : AppCompatActivity(), OnMapReadyCallback, SensorEventListener {
         }
     }
 
+    // Fonction nécessaire pour l'implémentation de l'accéléromètre
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
         return
     }
 
+    // A la fin de l'activité, on désenregistre le listener de l'accéléromètre et on arrête les timers task
     override fun onDestroy() {
         sensorManager.unregisterListener(this)
         super.onDestroy()
@@ -265,6 +233,7 @@ class Vue2 : AppCompatActivity(), OnMapReadyCallback, SensorEventListener {
         finish()
     }
 
+    // Fonction retournant les coordonnées du point de destination, en fonction de la distance et de l'angle
     fun getDestinationLatLng(originLatLng: LatLng, distance: Double, heading: Double): LatLng {
         // Utilise la méthode computeOffset de SphericalUtil pour calculer les coordonnées du point de destination
         return SphericalUtil.computeOffset(originLatLng, distance, heading)
